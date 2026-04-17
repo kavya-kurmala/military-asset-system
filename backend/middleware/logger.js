3a035580f1e36946d951ae1fab9018c001fdb7e5
kavya <kurmalakavya123@gmail.com>
2026-04-17 17:56:11 +0530

    Update logger.js

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -1,13 +1,7 @@
 const pool = require("../db");
 
-module.exports = async (req, res, next) => {
-  try {
-    await pool.query(
-      "INSERT INTO logs (action, details) VALUES ($1,$2)",
-      [req.method + " " + req.url, JSON.stringify(req.body)]
-    );
-  } catch (err) {
-    console.log("Logging error:", err.message);
-  }
+module.exports = (req, res, next) => {
+  console.log(`[LOG] ${req.method} ${req.url}`);
   next();
 };
+
